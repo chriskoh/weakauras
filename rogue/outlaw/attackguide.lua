82b0db83c7546bbd596f87927d939333f3d2fb6a
Chris Koh <chris.koh90@gmail.com>
2016-10-03 12:31:27 -0700

    Create attackguide.lua

---
+++ rogue/outlaw/attackguide.lua
@@ -0,0 +1,115 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Saber Slash (NB)",
+        [2] = "Opportunity (NB)",
+        [3] = "Run Through (NB)",
+        [4] = "Saber Slash (B)",
+        [5] = "Opportunity (B)",
+        [6] = "Run Through (B)",
+        [7] = "Crimson Vial",
+        [8] = "Blade Flurry",
+        [9] = "Roll (Recast)",
+        [10] = "Roll (Cast)",
+        [11] = "MfD",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["disjunctive"] = "all",
+    ["border"] = false,
+    ["untrigger"] = {
+    },
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["activeTriggerMode"] = -10,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["borderOffset"] = 5,
+    ["xOffset"] = 0,
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["id"] = "Pirate",
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["frameStrata"] = 1,
+    ["anchorPoint"] = "CENTER",
+    ["trigger"] = {
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["unit"] = "player",
+        ["debuffType"] = "HELPFUL",
+        ["names"] = {
+        },
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["yOffset"] = -180,
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["borderEdge"] = "None",
+    ["load"] = {
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "ROGUE",
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["use_class"] = "true",
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["expanded"] = false,
+}
