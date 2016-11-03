1c0339d9c2979056985666522d915b9ee9d3cc6e
Chris Koh <chris.koh90@gmail.com>
2016-11-02 17:32:29 -0700

    Update arms.lua

--- warrior/arms.lua
+++ warrior/arms.lua
@@ -1,1 +1,167 @@
 
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Execute Arms",
+        [2] = "Execute Arms Glow",
+        [3] = "Execute Arms NA",
+        [4] = "Focused Rage",
+        [5] = "Focused Rage at 3 stacks",
+        [6] = "Focused Rage NA",
+        [7] = "Mortal Strike",
+        [8] = "Mortal Strike Glow",
+        [9] = "Mortal Strike Glow Overpower",
+        [10] = "Mortal Strike CD",
+        [11] = "Colossus Smash",
+        [12] = "Colossus Smash CD",
+        [13] = "Warbreaker Master",
+        [14] = "Warbreaker CD",
+        [15] = "Whirlwind",
+        [16] = "Whirlwind Glow",
+        [17] = "Whirlwind NA",
+        [18] = "Rend Arms",
+        [19] = "Rend Arms NA",
+        [20] = "Rend Bar",
+        [21] = "Overpower",
+        [22] = "Overpower Glows",
+        [23] = "Slam",
+        [24] = "Slam NA",
+        [25] = "Cleave",
+        [26] = "Cleave CD",
+        [27] = "Battle Cry Arms",
+        [28] = "Battle Cry CD Arms",
+        [29] = "Battle Cry Bar Arms",
+        [30] = "Avatar Arms",
+        [31] = "Avatar CD Arms",
+        [32] = "Avatar Bar Arms",
+        [33] = "Bladestorm Arms",
+        [34] = "Bladestorm Arms Glow",
+        [35] = "Bladestorm Arms CD",
+        [36] = "Bladestorm Bar Arms",
+        [37] = "Tactician proc",
+        [38] = "Colossus Smash Bar",
+        [39] = "Mortal Strike Charges",
+        [40] = "Rage Bar Arms",
+        [41] = "Rage Bar Arms MS Tick",
+        [42] = "Rage Bar Arms MS Tick Dauntless",
+        [43] = "Rage Bar Arms  Deadly Calm",
+        [44] = "Rage Glow Arms",
+        [45] = "Rage Bar BG Arms",
+        [46] = "Enemies in Range Arms",
+        [47] = "Pummel Arms",
+        [48] = "Pummel Arms Glow",
+        [49] = "Pummel Arms CD",
+        [50] = "Heroic Leap CD Arms",
+        [51] = "Charge on CD Arms",
+        [52] = "Victory Rush Arms",
+        [53] = "Focused Rage 3 Stacks Bar",
+        [54] = "Focused Rage 2 Stacks Bar",
+        [55] = "Focused Rage 1 Stack Bar",
+        [56] = "Focused Rage Backdrop",
+        [57] = "FR 3 Stacks",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["disjunctive"] = "all",
+    ["border"] = false,
+    ["yOffset"] = 20,
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["activeTriggerMode"] = -10,
+    ["expanded"] = false,
+    ["xOffset"] = 10,
+    ["borderOffset"] = 5,
+    ["anchorPoint"] = "CENTER",
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["additional_triggers"] = {
+    },
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["frameStrata"] = 1,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+    ["trigger"] = {
+        ["unit"] = "player",
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["subeventSuffix"] = "_CAST_START",
+        ["debuffType"] = "HELPFUL",
+        ["names"] = {
+        },
+        ["event"] = "Health",
+        ["subeventPrefix"] = "SPELL",
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["borderEdge"] = "None",
+    ["id"] = "Arms - Khakhan",
+    ["load"] = {
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "WARRIOR",
+        },
+        ["use_class"] = "true",
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["untrigger"] = {
+    },
+}
